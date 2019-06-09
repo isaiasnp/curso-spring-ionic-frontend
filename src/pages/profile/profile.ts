081a9d602a3015f0f4d39c870f8855b784f4859f
Isaias Neres Pinto <isaias.neres@yahoo.com>
2019-06-09 04:01:25 -0300

    Componente de carregamento

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
@@ -18,24 +18,29 @@ export class ProfilePage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
+    let loader = this.presentLoading();
     if(localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
+          loader.dismiss();
         },
           error => {
+            loader.dismiss();
             if(error.status == 403){
               this.navCtrl.setRoot('HomePage');
             }
           });
     }
     else{
+      loader.dismiss();
       this.navCtrl.setRoot('HomePage');
     }
   }
@@ -46,6 +51,14 @@ export class ProfilePage {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
       },
       error => {});
+  }
+
+  presentLoading(){
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
   }
 
 }
